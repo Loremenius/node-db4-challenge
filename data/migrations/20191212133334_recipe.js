9c461c0ef1b7ab7754ecf91289e46df9b082b1ca
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2019-12-12 14:33:57 -0700

    added stipulations to columns (such as notNullable) and added foreign keys to knex and migrations columns

--- data/migrations/20191212133334_recipe.js
+++ data/migrations/20191212133334_recipe.js
@@ -3,22 +3,45 @@ exports.up = function(knex) {
   return knex.schema.createTable("recipies",tbl=>{
 
     tbl.increments();
-    tbl.string("name", 255);
+    tbl.string("name", 255)
+        .notNullable();
 
   }).createTable("instructions",tbl=>{
 
     tbl.increments();
-    tbl.string("instruction", 500);
-    tbl.integer("step");
-    tbl.integer("recipe_id");
+
+    tbl.string("instruction", 500)
+        .notNullable();
+
+    tbl.integer("step")
+        .notNullable();
+
+    tbl.integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable('recipies')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
     
 
   }).createTable("ingredients",tbl=>{
 
     tbl.increments();
-    tbl.string("name",255);
-    tbl.integer("amount");
-    tbl.integer("recipe_id");
+
+    tbl.string("name",255)
+        .notNullable();
+
+    tbl.integer("amount")
+        .notNullable();
+
+    tbl.integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable('recipies')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
 
   });
 };
--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,12 @@ module.exports = {
     client: 'sqlite3',
     useNullAsDefault:true,
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/recipie.db3'
+    },
+    pool:{
+      afterCreate:(conn, done)=>{
+        conn.run('PRAGMA foreign_keys = ON', done);
+      }
     },
     migrations:{
       directory:'./data/migrations'
