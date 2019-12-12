914667142a68f03de69fd3fb8812f2bf1268fdc6
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2019-12-12 14:08:22 -0700

    completed Migration so that it adds recipies, instructions, and ingredients with their according columns

--- data/migrations/20191212133334_recipe.js
+++ data/migrations/20191212133334_recipe.js
@@ -4,13 +4,28 @@ exports.up = function(knex) {
 
     tbl.increments();
     tbl.string("name", 255);
-    tbl.integer("ingredients_id");
-    tbl.integer("instructions_id");
 
+  }).createTable("instructions",tbl=>{
+
+    tbl.increments();
+    tbl.string("instruction", 500);
+    tbl.integer("step");
+    tbl.integer("recipe_id");
+    
+
+  }).createTable("ingredients",tbl=>{
+
+    tbl.increments();
+    tbl.string("name",255);
+    tbl.integer("amount");
+    tbl.integer("recipe_id");
 
   });
 };
 
 exports.down = function(knex) {
-    return knex.schema.dropTableIfExists("recipies");
+    return knex.schema
+        .dropTableIfExists("ingredients")
+        .dropTableIfExists("instructions")
+        .dropTableIfExists("recipies");
 };
